5e1c1b957128fc67b26b658f12d31de3e1bbcc12
Salman Yousaf <salmanyousaf240@gmail.com>
2018-10-04 22:27:16 +0500

    #2_Completed, Used rxjs and http.get() to call a github api and show result

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { switchMap, map, filter, debounceTime, distinct, tap } from 'rxjs/operators';
+
+interface IGithub{
+  items: Object;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+  searchTerm: string;
+  latestSearch = new Subject<string>();
+  loading: boolean;
+  results;
+
+  constructor(private http: HttpClient){
+    this.results = this.latestSearch
+    .pipe(debounceTime(500))
+    .pipe(distinct())
+    .pipe(filter(term => !!term))
+    .pipe(switchMap((term) =>
+      this.http.get(`https://api.github.com/search/repositories?q=${term}&sort=stars&order=desc`)
+      .pipe(map((res: IGithub) => res.items))
+      .pipe(tap(() => {
+        console.log("Http Service Called!");
+        this.loading = false; 
+      }))
+    ));
+  }
+
+  newSearch(term: string){
+    this.latestSearch.next(term);
+    this.loading = true;
+  }
+
+}//class ends.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatCardModule, MatToolbarModule, MatInputModule, MatSnackBarModule, MatProgressSpinnerModule } 
+  from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, FormsModule, MatButtonModule, MatCardModule, MatToolbarModule,
+    MatInputModule, MatSnackBarModule, HttpClientModule, MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
